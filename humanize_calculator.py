c307ce4180589742e47e117e850626450653d3bd
Slavik Kovbasa <slavikovbasa@gmail.com>
2019-05-19 19:32:53 +0300

    Add is_valid and corresponding tests

--- humanize_calculator.py
+++ humanize_calculator.py
@@ -1,32 +1,93 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import re
 
 class InvalidCharError(ValueError): pass
 
 operators: str = '+-*/='
 
-def extract_numeric(string: str) -> Tuple[str, str]:
-    ''' Extract longest numeric string from the beginning of the input string
-        
+human_numerics: Dict[str, str] = {
+    '0': 'zero',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
+    '10': 'ten',
+    '11': 'eleven',
+    '12': 'twelve',
+    '13': 'thirteen',
+    '14': 'fourteen',
+    '15': 'fifteen',
+    '16': 'sixteen',
+    '17': 'seventeen',
+    '18': 'eighteen',
+    '19': 'nineteen',
+    '20': 'twenty',
+    '30': 'thirty',
+    '40': 'fourty',
+    '50': 'fifty',
+    '60': 'sixty',
+    '70': 'seventy',
+    '80': 'eighty',
+    '90': 'ninety',
+    '100':'hundred',
+    '1000': 'thousand',
+    '1000000': 'million',
+    '1000000000': 'billion'
+}
+
+human_operators: Dict[str, str] = {
+    '+': 'plus',
+    '-': 'minus',
+    '*': 'multiplied by',
+    '/': 'divided by',
+    '=': 'equals'
+}
+
+expression_pattern = re.compile(
+    r'''^(0|[1-9]\d*)       # the first token is always number;
+                            # number is either single '0' character
+                            # or any other numeric string,
+                            # that doesn't start with 0
+        (               
+            [\+\-\*\/\=]    # any operator
+            (0|[1-9]\d*)    # any number
+        )*$                 # pattern <operator><number> is repeated
+                            # arbitrary amount of times
+    ''', re.VERBOSE
+)
+
+def is_valid(expression: str) -> bool:
+    '''Test if expression is valid'''
+    return expression_pattern.match(expression) != None
+
+def extract_token(expression: str) -> Tuple[str, str]:
+    '''Extract token from the beginning of the expression
+
+    Returns a tuple(<extracted token>, <expression remainder>).
+    Handles numeric tokens and tokens from the operators string.
+    If unknown char is found, InvalidCharError is raised.
     '''
-    if string == '' or not string[0].isdigit():
-        return ('', string)
+    if expression == '':
+        return ('', expression)
+    elif expression[0].isdecimal():
+        match = re.match(r'(\d+)(.*)', expression)
+        return match.groups()
+    elif expression[0] in operators:
+        return (expression[0], expression[1:])
     else:
-        rest: Tuple[str, str] = extract_numeric(string[1:])
-        return (string[0] + rest[0], rest[1])
+        raise InvalidCharError('Invalid char {}'.format(expression[0]))
 
-def extract_token(string: str) -> Tuple[str, str]:
-    if string == '':
-        return ('', string)
-    elif string[0].isdigit():
-        return extract_numeric(string)
-    elif string[0] in operators:
-        return (string[0], string[1:])
-    else:
-        raise InvalidCharError("Invalid char {}".format(string[0]))
+def tokenize(expression: str) -> List[str]:
+    '''Tokenize expression
 
-def split_expression(expression: str) -> List[str]:
-    expression = expression.replace(' ', '')
+    Returns a list of all tokens in expression.
+    If unknown char is found, InvalidCharError is raised.
+    '''
     elements: List[str] = []
     while expression != '':
         token, expression = extract_token(expression)
--- test_humanize_calculator.py
+++ test_humanize_calculator.py
@@ -3,18 +3,51 @@ import pytest
 import random
 from typing import Iterator, Tuple, List
 
-def random_expression_and_split(amount: int) -> Iterator[Tuple[str, List[str]]]:
-    for _ in range(amount):
-        elements: List[str] = []
-        elements.append(str(random.randrange(100)))
-        for _ in range(random.randrange(10)):
-            elements.append(random.choice(humanize_calculator.operators))
-            elements.append(str(random.randrange(100)))
-        expression = elements[0]
-        for el in elements[1:]:
-            expression += ' ' * random.randrange(5) + el
-        yield (expression, elements)
+invalid_expressions: List[str] = [
+    '00', '000', '01', '002', '0123', '+', '-', '*', '/', '=',
+    '2+', '2-', '2*', '2/', '2=', '+2', '-2', '*2', '/2', '=2',
+    '2++2', '2--2', '2**2', '2//2', '2==2', '2+++2', '2---2',
+    '2***2', '2///2', '2===2', '2+2==2', '2++2=2', '2--2=0',
+    '2**2=4', '2//2=1', '2+2==2+2', '.', '(', ')', ',', '?', '!',
+    '|', '&', '^', '%', '2.+2=4', '2+2.=4', '2+2=4.', '0.2+2=2',
+    'A', 'a', 'B', 'C', 'D', 'E', 'F'
+]
 
-@pytest.mark.parametrize('expression,expression_split', random_expression_and_split(100))
-def test_random_expression_split(expression, expression_split):
-    assert humanize_calculator.split_expression(expression) == expression_split
+valid_expressions_humanize: List[str] = [
+    ("0", "zero"),
+    ("1", "one"),
+    ("2", "two"),
+    ("3", "three"),
+    ("4", "four"),
+    ("5", "five"),
+    ("6", "six"),
+    ("7", "seven"),
+    ("8", "eight"),
+    ("9", "nine"),
+
+]
+
+def random_expression_and_tokens(amount: int = 100) -> Iterator[Tuple[str, List[str]]]:
+    for _ in range(amount):
+        tokens: List[str] = []
+        tokens.append(str(random.randrange(100)))
+        for _ in range(random.randrange(10)):
+            tokens.append(random.choice(humanize_calculator.operators))
+            tokens.append(str(random.randrange(100)))
+        yield (''.join(tokens), tokens)
+
+@pytest.mark.parametrize('expression,tokens', random_expression_and_tokens())
+def test_is_valid_true(expression,tokens):
+    assert humanize_calculator.is_valid(expression)
+    
+@pytest.mark.parametrize('expression', invalid_expressions)
+def test_is_valid_false(expression):
+    assert not humanize_calculator.is_valid(expression)
+
+@pytest.mark.parametrize('expression,tokens', random_expression_and_tokens())
+def test_random_tokenize(expression, tokens):
+    assert humanize_calculator.tokenize(expression) == tokens
+
+# @pytest.mark.parametrize('expression,human_string', valid_expression_humanize)
+# def test_humanize():
+#     assert humanize_calculator.humanize()
